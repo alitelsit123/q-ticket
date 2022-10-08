9d6ea4c8bdf937a8c4b4c745d5d7641316c7260d
reza <reza.eduwork@gmail.com>
2022-10-08 22:52:45 +0700

    r

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,9 +11,11 @@ export default function Home(props) {
     <div className='w-[90vw] h-[90vh] mx-auto bg-white p-8'>
       <div className='flex justify-center space-x-16 p-16'>
         <a href='https://docs.google.com/forms/d/e/1FAIpQLSdN0VlLoLFxpDiSCQhu5k1f0kyFxQKC87TDZAR3VhHyQTLWMg/viewform?usp=sharing' 
-        className='h-[150px] w-[150px] border bg-gray-300 border-gray-300 rounded-full' target={'_blank'}></a>
+        className='h-[150px] w-[150px] border bg-gray-300 border-gray-300 rounded-full font-black flex items-center justify-center' target={'_blank'}>
+          <span className='mx-auto text-center'>Form Inspect Network</span>
+        </a>
         <a href='https://docs.google.com/forms/d/e/1FAIpQLSc29ltbo24_-YmSF8ZoA7Au2U75rJceEq9D9Dc9OTp-t1AWWQ/viewform?usp=sharing' 
-        className='h-[150px] w-[150px] border bg-gray-300 border-gray-300 rounded-full' target={'_blank'}></a>
+        className='h-[150px] w-[150px] border bg-gray-300 border-gray-300 rounded-full font-black flex items-center justify-center' target={'_blank'}>Form Otk</a>
       </div>
       <div className='flex justify-center space-x-16'>
         <button type='button'
@@ -23,7 +25,7 @@ export default function Home(props) {
           title: 'Inspect Jaringan',
           data: inspectNetwork
         })} 
-        className='h-[80px] w-[150px] border bg-gray-300 border-gray-300'></button>
+        className='h-[80px] w-[150px] border bg-gray-300 border-gray-300 font-black'>Inspect Network</button>
         <button type='button'
         onClick={() => setDialog({
           open: true,
@@ -31,7 +33,7 @@ export default function Home(props) {
           title: 'Loogbook OTK',
           data: otk
         })} 
-        className='h-[80px] w-[150px] border bg-gray-300 border-gray-300'></button>
+        className='h-[80px] w-[150px] border bg-gray-300 border-gray-300 font-black'>OTK</button>
       </div>
     </div>
   )
